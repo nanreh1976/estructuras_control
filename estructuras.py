49cb92c61f7ee2043f24677fa2e5ffb5849e3743
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-10-09 14:58:18 -0300

    estructura de control en python

---
+++ estructuras.py
@@ -0,0 +1,18 @@
+#estructuras de control
+
+edad = int(input("ingresa tu edad "))
+
+if (edad >= 18) :
+  print("hola")
+  print("tu edad es mayor es: " , edad)
+elif (edad >=16) :
+   print("podes votar ")
+elif (edad >= 10) :
+    print("tenes mas de 10 y menor de 16")  
+else:
+    print("como te va ?")
+    print("como")
+    print("sos menor")
+    print("tu edad es ",edad)
+
+print("chau !")
